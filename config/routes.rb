702cc71b5253a034ecda763b7d296ae37d05266e
saqib010 <010saqib@gmail.com>
2021-11-05 14:19:13 +0500

    integrated Pages with custome routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+ 
+  #get 'pages/home'
+  #get 'pages/about'
+  get 'aboutme',to:'pages#about' #first is name second is path # we can change first as we like
+  #get 'pages/contact'
+  get 'contact',to:'pages#contact' #we can assign any name to page to access in URL
+  
+  root to:'pages#home' #home page is now our first page at http://localhost:3000/
+
+
+
   resources :portfolios
   resources :blogs
   resources :skills
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
 end
